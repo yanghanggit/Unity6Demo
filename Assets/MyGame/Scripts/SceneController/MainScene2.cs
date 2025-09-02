75a7ed70fc963b2608e0024957689794486d09ab
yanghanggit <1220826563@qq.com>
2025-09-02 14:14:21 +0800

    调整代码名字。

---
+++ Assets/MyGame/Scripts/SceneController/MainScene2.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+public class MainScene2 : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    public void OnClickBack()
+    {
+        Debug.Log("Back button clicked");
+    }
+
+    public void OnClickCamp()
+    {
+        Debug.Log("OnClickCamp");
+    }
+
+    public void OnClickDungeon()
+    {
+        Debug.Log("OnClickDungeon");
+    }
+}
