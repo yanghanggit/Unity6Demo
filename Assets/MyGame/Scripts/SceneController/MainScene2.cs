d5706eafa512b71105b6a25662fae7ffb787be2e
yanghanggit <1220826563@qq.com>
2025-09-02 14:21:20 +0800

    连接进入。

--- Assets/MyGame/Scripts/SceneController/MainScene2.cs
+++ Assets/MyGame/Scripts/SceneController/MainScene2.cs
@@ -1,14 +1,25 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 public class MainScene2 : MonoBehaviour
 {
+
+    public string _preScene = "LoginScene";
+
+    public LogoutAction _logoutAction;
+
+    
+
     void Start()
     {
+        Debug.Assert(_logoutAction != null, "_logoutAction is null");
 
     }
 
     public void OnClickBack()
     {
         Debug.Log("Back button clicked");
+        StartCoroutine(ReturnToLoginScene());
     }
 
     public void OnClickCamp()
@@ -19,5 +30,19 @@ public class MainScene2 : MonoBehaviour
     public void OnClickDungeon()
     {
         Debug.Log("OnClickDungeon");
+    }
+
+
+    IEnumerator ReturnToLoginScene()
+    {
+        yield return _logoutAction.Call();
+
+        if (!_logoutAction.LastRequestSuccess)
+        {
+            Debug.LogError("LogoutAction request failed");
+            yield break;
+        }
+
+        SceneManager.LoadScene(_preScene);
     }
 }
