8d879947cb24444852cf74ede4460f456443eff0
yanghanggit <1220826563@qq.com>
2025-11-17 16:56:45 +0800

    删文件！

--- Assets/MyGame/Scripts/Models/world.cs
+++ Assets/MyGame/Scripts/Models/world.cs
@@ -1,16 +1,1 @@
-
-using System.Collections.Generic;
-
-[System.Serializable]
-public class BaseMessage
-{
-    public string content = "";
-    public string type = "";
-}
-
-[System.Serializable]
-public class AgentChatHistory
-{
-    public string name = "";
-    public List<BaseMessage> chat_history = new List<BaseMessage>();
-}
+//???
