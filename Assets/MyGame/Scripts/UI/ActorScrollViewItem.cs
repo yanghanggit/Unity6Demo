a88d62439aa2bdbd8a29645e024b590a070730e7
yanghanggit <1220826563@qq.com>
2025-11-30 17:50:58 +0800

    添加文件！

---
+++ Assets/MyGame/Scripts/UI/ActorScrollViewItem.cs
@@ -0,0 +1,26 @@
+namespace Mosframe {
+
+    //using System.Collections.Generic;
+    using UnityEngine;
+    using UnityEngine.EventSystems;
+    using UnityEngine.UI;
+
+    public class ActorScrollViewItem : UIBehaviour, IDynamicScrollViewItem 
+    {
+	    private readonly Color[] colors = new Color[] {
+		    Color.cyan,
+		    Color.green,
+	    };
+
+	    public Image icon;
+	    public Text  title;
+	    public Image background;
+
+        public void onUpdateItem( int index ) {
+
+		    this.title.text         = string.Format("Actor{0:d3}", (index + 1) );
+		    this.background.color   = this.colors[Mathf.Abs(index) % this.colors.Length];
+		    this.icon.sprite        = Resources.Load<Sprite>( (Mathf.Abs(index) % 20 + 1).ToString("icon_00") );
+        }
+    }
+}
