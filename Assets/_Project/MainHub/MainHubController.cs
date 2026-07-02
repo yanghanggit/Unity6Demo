d69f4505fbbb14ec02b36cdffba1c9eba02720b2
yanghanggit <1220826563@qq.com>
2026-07-02 12:28:48 +0800

    添加代码调用

--- Assets/_Project/MainHub/MainHubController.cs
+++ Assets/_Project/MainHub/MainHubController.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 
 /*
@@ -31,15 +32,27 @@ TabContentPanel 下的子 Panel 等功能确认后可改为 ShopPanel、BagPanel
 
 public class MainHubController : MonoBehaviour
 {
+    [Header("UI Components")]
+    [SerializeField] private TMP_Text[] _navTabButtonTexts; // 导航标签按钮文本数组，按顺序对应标签顺序
+
+
+    void Awake()
+    {
+        Debug.Assert(_navTabButtonTexts != null && _navTabButtonTexts.Length == 5, "_navTabButtonTexts is null or empty");
+    }
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        for (int i = 0; i < _navTabButtonTexts.Length; i++)
+        {
+            _navTabButtonTexts[i].text = $"标签{i + 1}"; // TODO: 待替换为实际标签名
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnNavTabButtonClicked(int tabIndex)
     {
-        
+        Debug.Log($"导航标签按钮 {tabIndex} 被点击");
     }
 }
---
+++ Assets/_Project/MainHub/TopHUDBarController.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+// using Cysharp.Threading.Tasks;
+// using UnityEngine;
+// using UnityEngine.SceneManagement;
+using TMPro;
+
+public class TopHUDBarController : MonoBehaviour
+{
+    [Header("UI Components")]
+    [SerializeField] private TMP_Text _resourcesText;
+    [SerializeField] private TMP_Text _playerAvatarText;
+
+
+    void Awake()
+    {
+        Debug.Assert(_resourcesText != null, "_resourcesText is null");
+        Debug.Assert(_playerAvatarText != null, "_playerAvatarText is null");
+    }
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _resourcesText.text = "资源: 999/999"; // TODO: 待替换为实际资源数据
+        _playerAvatarText.text = "玩家头像";
+    }
+
+    // Update is called once per frame
+    // void Update()
+    // {
+
+    // }
+
+    public void OnClickPlayerAvatar()
+    {
+        Debug.Log("点击了玩家头像");
+    }
+}
